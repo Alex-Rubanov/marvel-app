f51bef09c901007c44910c9570a845e70b368c7b
Alex <rubanovv.alex@gmail.com>
2023-01-23 23:46:33 +0100

    added demo get request for random character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -6,7 +6,7 @@ class MarvelService {
         let res = await fetch(url);
 
         if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.starus}`);
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
         return await res.json();
